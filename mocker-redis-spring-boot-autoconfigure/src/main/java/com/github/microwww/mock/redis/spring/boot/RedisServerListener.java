fb22aa74f1b34e0f1a9aa0bed234b3ae1910fb5e
changshu.li <lcs.005@163.com>
2020-07-12 23:54:24 +0800

    redis - microwww.redis

--- mocker-redis-spring-boot-autoconfigure/src/main/java/com/github/microwww/mock/redis/spring/boot/RedisServerListener.java
+++ mocker-redis-spring-boot-autoconfigure/src/main/java/com/github/microwww/mock/redis/spring/boot/RedisServerListener.java
@@ -1,6 +1,6 @@
 package com.github.microwww.mock.redis.spring.boot;
 
-import com.github.fppt.jedismock.RedisServer;
+import com.github.microwww.redis.RedisServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -16,21 +16,12 @@ public class RedisServerListener {
 
     private static final Logger logger = LoggerFactory.getLogger(RedisServerListener.class);
 
-    @Bean(destroyMethod = "stop")
-    public RedisServer mockRedisServer(RedisProperties redisProperties) {
-        RedisServer server = this.start(redisProperties);
-        logger.info("Mocker Redis start :: [{}:{}], set 'server.redis.host' to match it", server.getHost(), server.getBindPort());
+    @Bean(destroyMethod = "close")
+    public RedisServer mockRedisServer(RedisProperties redisProperties) throws IOException {
+        RedisServer server = new RedisServer();
+        server.listener(redisProperties.getHost(), redisProperties.getPort());
+        logger.info("Mocker Redis start :: [{}:{}], set 'server.redis.host' to match it", redisProperties.getHost(), redisProperties.getPort());
         return server;
-    }
-
-    public synchronized RedisServer start(RedisProperties redisProperties) {
-        try {
-            RedisServer server = RedisServer.newRedisServer(redisProperties.getPort());  // bind to a random port
-            server.start();
-            return server;
-        } catch (IOException e) {
-            throw new RuntimeException("Start redis error !", e);
-        }
     }
 
 }
